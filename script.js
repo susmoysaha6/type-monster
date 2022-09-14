41bd19cfaf8bf252a9cdb5a55688f30f48f421a7
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-15 05:59:51 +0600

    blog page created and question one answered

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
-    errorCount++
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
